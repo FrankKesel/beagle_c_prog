7479737e5b79167e8631918535e6cdd17cfdc361
Frank Kesel <frank.kesel@hs-pforzheim.de>
2016-01-10 19:36:33 +0100

    First version of motion detection

--- opencv_t1/src/boneCV.cpp
+++ opencv_t1/src/boneCV.cpp
@@ -1,8 +1,3 @@
-/* Basic Image Processing Example using OpenCV
-* Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
-* Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
-* ISBN 9781118935125. Please see the file README.md in the repository root 
-* directory for copyright and GNU GPLv3 license information.            */
 
 #include<iostream>
 #include<opencv2/opencv.hpp>   // C++ OpenCV include file
@@ -11,33 +6,45 @@ using namespace cv;            // using the cv namespace too
 
 int main()
 {
-    VideoCapture capture(0);   // capturing from /dev/video0
+	VideoCapture capture(0);   // capture object, 0: only one cam available
+   Mat frame, g1, g2, result;    
 
-    cout << "Started Processing - Capturing Image" << endl;
-    // set any  properties in the VideoCapture object
-    capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);   // width pixels
-    capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);   // height pixels
-    capture.set(CV_CAP_PROP_GAIN, 0);            // Enable auto gain etc.
-    if(!capture.isOpened()){   // connect to the camera
-       cout << "Failed to connect to the camera." << endl;
-    }
+   cout << "Started Processing - Capturing Image" << endl;
+   // Set properties in the VideoCapture object: resolution
+   capture.set(CV_CAP_PROP_FRAME_WIDTH,800);   // width pixels
+   capture.set(CV_CAP_PROP_FRAME_HEIGHT,600);   // height pixels
+   if(!capture.isOpened()){   // check if cam is connected
+   	cout << "Failed to connect to the camera." << endl;
+   	return -1;
+   }
 
-    Mat frame, gray, edges;    // images for orignal, grayscale and edge image
-    capture >> frame;          // capture the image to the frame
-    if(frame.empty()){         // did the capture succeed?
-       cout << "Failed to capture an image" << endl;
-       return -1;
-    }
-    cout << "Processing - Performing Image Processing" << endl;
-    cvtColor(frame, gray, CV_BGR2GRAY); // convert to grayscale
-    blur(gray, edges, Size(3,3));       // blur grayscale image 3x3 kernel
-    // use Canny edge detector that outputs to the same image
-    // low threshold = 10, high threshold = 30, kernel size = 3
-    Canny(edges, edges, 10, 30, 3);      // Run Canny edge detector
-    cout << "Finished Processing - Saving images" << endl;
+	capture.read(frame); //Capture image from cam    
+   if(frame.empty()){         
+      cout << "Failed to capture an image" << endl;
+      return -1;
+   }
+   cvtColor(frame, g1, CV_BGR2GRAY); // convert to grayscale
 
-    imwrite("capture.png", frame);
-    imwrite("grayscale.png", gray);
-    imwrite("edges.png", edges);
-    return 0;
+	capture.read(frame); //Capture image from cam    
+   if(frame.empty()){         
+      cout << "Failed to capture an image" << endl;
+      return -1;
+   }
+   cvtColor(frame, g2, CV_BGR2GRAY); // convert to grayscale
+   
+   subtract(g1, g2, result);
+   
+   Mat D(result, Rect(400,400,10,10));
+   cout << "D: " << endl << D << endl;
+
+	double s = sum(result)[0];
+	s = s/480000;
+	cout <<"Charakteristik: "<< s <<endl;
+	  
+
+   imwrite("../img/capture.png", frame);
+   imwrite("../img/g1.png", g1);
+   imwrite("../img/g2.png", g2);
+   imwrite("../img/result.png", result);
+   return 0;
 }
